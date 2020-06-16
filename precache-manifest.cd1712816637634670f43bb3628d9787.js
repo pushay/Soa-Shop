93fd390e4f4875f90f5110b1982f8c11207ec005
pushay <sobocinskapraca@gmail.com>
2020-06-16 10:26:17 +0200

    Updates

---
+++ precache-manifest.cd1712816637634670f43bb3628d9787.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f451db0aeacaed6d7bcdb57672f335a",
+    "url": "/Soa-Shop/index.html"
+  },
+  {
+    "revision": "7caf84119b388a2652eb",
+    "url": "/Soa-Shop/static/css/main.10b4c2ac.chunk.css"
+  },
+  {
+    "revision": "eb9c3af71b2e14f880cd",
+    "url": "/Soa-Shop/static/js/2.5b1691c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Soa-Shop/static/js/2.5b1691c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7caf84119b388a2652eb",
+    "url": "/Soa-Shop/static/js/main.e81ba6be.chunk.js"
+  },
+  {
+    "revision": "d412ee9f5cb2e54ca4d7",
+    "url": "/Soa-Shop/static/js/runtime-main.4099aa0e.js"
+  },
+  {
+    "revision": "7c2fb1010a717874fb74c20e55eda0ae",
+    "url": "/Soa-Shop/static/media/cloth1.7c2fb101.jpg"
+  },
+  {
+    "revision": "6b5af16e445fde8ac94a7ed79ec1503d",
+    "url": "/Soa-Shop/static/media/cloth10.6b5af16e.jpg"
+  },
+  {
+    "revision": "4e2742bce48ad772e05cb0f3075e1546",
+    "url": "/Soa-Shop/static/media/cloth11.4e2742bc.jpg"
+  },
+  {
+    "revision": "5ba87e0c92a980efd203d7ab14b99e43",
+    "url": "/Soa-Shop/static/media/cloth12.5ba87e0c.jpg"
+  },
+  {
+    "revision": "f1e7b0697689170a56456a8734900bd6",
+    "url": "/Soa-Shop/static/media/cloth13.f1e7b069.jpg"
+  },
+  {
+    "revision": "44bb7994e0177ad400b8c27680b8627d",
+    "url": "/Soa-Shop/static/media/cloth14.44bb7994.jpg"
+  },
+  {
+    "revision": "36237173bea529b4c661c1f548addad4",
+    "url": "/Soa-Shop/static/media/cloth2.36237173.jpg"
+  },
+  {
+    "revision": "5245c72ee45bc9466e3db6e2de056270",
+    "url": "/Soa-Shop/static/media/cloth3.5245c72e.jpg"
+  },
+  {
+    "revision": "3bc4dfd71908c7acd60ca425b789adcc",
+    "url": "/Soa-Shop/static/media/cloth4.3bc4dfd7.jpg"
+  },
+  {
+    "revision": "f3a98e5820b5590e291b2a9baf22d020",
+    "url": "/Soa-Shop/static/media/cloth5.f3a98e58.jpg"
+  },
+  {
+    "revision": "91edd0715890b7205d7e9d543749018a",
+    "url": "/Soa-Shop/static/media/cloth6.91edd071.jpg"
+  },
+  {
+    "revision": "05cdc662a48b377877e672b51844dcf5",
+    "url": "/Soa-Shop/static/media/cloth7.05cdc662.jpg"
+  },
+  {
+    "revision": "dcc4dcaa27bb87df5399ec06058d83a8",
+    "url": "/Soa-Shop/static/media/cloth8.dcc4dcaa.jpg"
+  },
+  {
+    "revision": "7b2eef6b39d8ed3a6119eee19de61d9b",
+    "url": "/Soa-Shop/static/media/cloth9.7b2eef6b.jpg"
+  },
+  {
+    "revision": "7f8afd684b96408e09f752ce3192fab6",
+    "url": "/Soa-Shop/static/media/joshua.7f8afd68.jpg"
+  },
+  {
+    "revision": "66d57a1bd66ca0ba029d4a1e573f7f2e",
+    "url": "/Soa-Shop/static/media/shopping.66d57a1b.jpg"
+  }
+]);
